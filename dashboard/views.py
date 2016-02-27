622af016d7e5cc072e0fc5155fdac00b09b39ea0
kishan <kspatel2018@gmail.com>
2016-02-27 03:45:51 -0500

    major key update

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+
+
+# from .models import Choice, Question
+
+
+
+def index(request):
+    # upcoming_appointments = Doctor.objects.order_by('-pub_date')[:5]
+    upcoming_appointments = "temp"
+    context = {'upcoming_appointments': upcoming_appointments}
+    return render(request, 'dashboard/index.html', context)
+
+
+def profile(request):
+    # upcoming_appointments = Doctor.objects.order_by('-pub_date')[:5]
+    profile = "temp"
+    context = {'profile': profile}
+    return render(request, 'dashboard/profile.html', context)
+
+
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
