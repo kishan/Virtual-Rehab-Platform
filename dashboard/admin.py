64f64248d711a7045ccd6b159813ba21d7e6fdd5
kishan <kspatel2018@gmail.com>
2016-03-04 22:54:53 -0500

    edit admin interface

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -2,13 +2,10 @@ from django.contrib import admin
 
 from .models import Patient, Doctor
 
-
-
-
 class PatientAdmin(admin.ModelAdmin):
     model = Patient
-    list_display = ('full_name', 'age', 'gender', 'previous_injuries', 'recovery_percentage', 'doctor')
-    list_filter = ['last_name', 'DOB', 'recovery_percentage', 'doctor']
+    list_display = ('first_name','last_name', 'age', 'gender', 'phone_number','recovery_percentage', 'doctor')
+    list_filter = ['first_name','last_name', 'DOB', 'recovery_percentage', 'doctor']
     search_fields = ['full_name']
     readonly_fields = ["age",]
 
